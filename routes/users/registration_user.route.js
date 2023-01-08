e8ddc50814d9735e24008bf36a02891e431883a7
Rashidul Hasan <rhratul12@gmail.com>
2023-01-09 02:42:06 +0600

    Removed unwanted file

--- routes/users/registration_user.route.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userRegistration = require("../../controllers/users/registration_user.controller");
-router.post("/registration", userRegistration);
-
-module.exports = router;
