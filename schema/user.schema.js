e9dc848f49ee03c4383b6434a70026781466eb62
Rashidul Hasan <rhratul12@gmail.com>
2023-01-09 00:23:14 +0600

    create user schema

--- schema/user.schema.js
+++ schema/user.schema.js
@@ -1,8 +1,42 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required : true,
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validator(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is Invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 4,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password Cannot contain password");
+      }
+    },
+  },
+
+  businessType: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
