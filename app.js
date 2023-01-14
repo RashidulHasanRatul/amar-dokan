05b00b9515bc2546939a22ecd38b6d2cfd5a07da
Rashidul Hasan <rhratul12@gmail.com>
2023-01-15 03:41:07 +0600

    add dotenv package

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 const bodyParser = require("body-parser");
 const pageNotFound = require("./middleware/404_middleware");
+const check_login = require("./middleware/check_login.middleware");
 const homePage = require("./routes/home/homePage");
 const user = require("./routes/users/user.route");
 const customer = require("./routes/customers/customer.route");
+dotenv.config();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(homePage, user, customer);
 app.use(pageNotFound);
+// app.use(check_login);
+
 module.exports = { app };
