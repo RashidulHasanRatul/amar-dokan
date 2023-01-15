e7892b5c4a18bdc5710104de092cebfb3775b2ff
Rashidul Hasan <rhratul12@gmail.com>
2023-01-15 20:04:24 +0600

    add auth to the endpoint

--- routes/customers/customer.route.js
+++ routes/customers/customer.route.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const check_login = require("../../middleware/check_login.middleware");
 const {
   customerRegistration,
   getAllCustomers,
 } = require("../../controllers/customers/customers.controller");
 
-router.post("/customers", customerRegistration);
-router.get("/customers", getAllCustomers);
+// customer Registration : POST
+router.post("/customers", check_login, customerRegistration);
+// Customer Get All :GET
+router.get("/customers", check_login, getAllCustomers);
 module.exports = router;
