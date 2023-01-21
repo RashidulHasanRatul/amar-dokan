73e57157bc88c274f1712441da19daed7515c590
Rashidul Hasan <rhratul12@gmail.com>
2023-01-22 03:38:26 +0600

    updated with user Id

--- middleware/check_login.middleware.js
+++ middleware/check_login.middleware.js
@@ -5,7 +5,7 @@ const check_login = async (req, res, next) => {
     const token = req.header("Authorization").replace("Bearer ", "");
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({
-      _id: decoded._id,
+      userId: decoded.userId,
     });
     req.user = user;
     next();
