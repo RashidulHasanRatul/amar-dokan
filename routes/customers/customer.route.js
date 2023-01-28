b858b97fd4364a05e1a86cbe877e2d3300878608
Rashidul Hasan <rhratul12@gmail.com>
2023-01-29 03:54:26 +0600

    add get customer by user id

--- routes/customers/customer.route.js
+++ routes/customers/customer.route.js
@@ -4,10 +4,12 @@ const check_login = require("../../middleware/check_login.middleware");
 const {
   customerRegistration,
   getAllCustomers,
+  getCustomer,
 } = require("../../controllers/customers/customers.controller");
 
 // customer Registration : POST
 router.post("/customers", check_login, customerRegistration);
 // Customer Get All :GET
 router.get("/customers", check_login, getAllCustomers);
+router.get("/customers/:userId", check_login, getCustomer);
 module.exports = router;
