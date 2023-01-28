a6a19e6e9c477028b31b26013608d3cf5b2d4730
Rashidul Hasan <rhratul12@gmail.com>
2023-01-29 03:54:58 +0600

    add get customer by id add

--- controllers/customers/customers.controller.js
+++ controllers/customers/customers.controller.js
@@ -1,7 +1,9 @@
 const Customer = require("../../schema/customers/customers.schema");
+
 const customerRegistration = async (req, res) => {
   const { customerName, mobileNumber, address, productType, price } = req.body;
   const customer = new Customer({
+    userId: req.user.userId,
     customerName: customerName,
     mobileNumber: mobileNumber,
     address: address,
@@ -21,4 +23,14 @@ const getAllCustomers = (req, res) => {
     }
   });
 };
-module.exports = { customerRegistration, getAllCustomers };
+
+const getCustomer = async (req, res) => {
+  try {
+    const customers = await Customer.find({ userId: req.user.userId });
+    return res.status(200).json({ customers });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+module.exports = { customerRegistration, getAllCustomers, getCustomer };
